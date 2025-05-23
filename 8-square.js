e7a02b0b49d7dd406934c3992650b7ae1e851a93
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-05-24 00:27:51 +0300

    8. Square

--- 8-square.js
+++ 8-square.js
@@ -1,4 +1,10 @@
-for (let i = 0; process.argv[2] > i; i++) {
-    console.log("X".repeat(process.argv[2]))
+if (process.argv[2] && !isNaN(parseInt(process.argv[2]))) {
+    for (let i = 0; process.argv[2] > i; i++) {
+        console.log("X".repeat(process.argv[2]))
+    }
+} else {
+    console.log("Missing size")
 }
+
+
 
