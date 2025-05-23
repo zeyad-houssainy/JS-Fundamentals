b00b2582c67d7bcdb1985ab93a0886f5cdfbe4e7
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-05-23 15:20:39 +0300

    task 3

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,5 +1,6 @@
 if (process.argv[2]) {
-	process.argv.slice(2).forEach(arg => console.log(arg));
+	// process.argv.slice(2).forEach(arg => console.log(arg)); use this to print all argv
+    console.log(process.argv[2])
 } else {
 	console.log('No argument');
 }
